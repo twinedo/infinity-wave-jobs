dc73d42d73dcf31a03cb9a88726b62d446516de2
Twin Edo <twinedo.dev@gmail.com>
2025-07-01 00:00:40 +0700

    add hero

---
+++ src/features/hero/index.tsx
@@ -0,0 +1,12 @@
+export const Hero = () => {
+  return (
+    <div className="flex flex-col justify-center w-full max-w-7xl gap-y-4 items-center h-[320px]">
+      <div className="text-black text-6xl font-bold">
+        Welcome to Infinity Wave Jobs
+      </div>
+      <div>
+        Where you find Jobs easily
+      </div>
+    </div>
+  );
+};
